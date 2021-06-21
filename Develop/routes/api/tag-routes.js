ab3c5ad60f271dfb7708f14f9b3075c44e8d61d2
jose <jmolina721@gmail.com>
2021-06-20 23:53:14 -0500

    Added tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { toDefaultValue } = require('sequelize/types/lib/utils');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -6,15 +7,60 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.params.tag_name
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +69,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  // Tag.destroy({
+  //   where: {
+  //     id: req.params.id
+  //   }
+  // })
+  // .then(dbTagData => {
+  //   if (!dbTagData) {
+  //   res.status(404).json({ message: 'No tag found with this id' });
+  //   return;
+  //   }
+  //   res.json(dbTagData);
+  // })
+  // .catch(err => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  // });
 });
 
 module.exports = router;
