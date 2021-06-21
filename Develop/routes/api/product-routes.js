11869f51321006625d48eab56df3a28e28389f64
jose <jmolina721@gmail.com>
2021-06-21 14:38:29 -0500

    Finished product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -140,7 +140,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
